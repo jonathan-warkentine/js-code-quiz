d2b0ede7a0138e33fb97ed7c47b3c975efc691a6
Jonathan Warkentine <jonathan.warkentine@gmail.com>
2022-01-26 00:11:02 -0500

    added a fade-out feature for the popup messages

--- assets/js/script.js
+++ assets/js/script.js
@@ -67,202 +67,219 @@ var whichQuestion = 0;
 gamestartCardEl.setAttribute("style", "display: flex");
 
 
-/* G A M E   F U N C T I O N S */
-function startGame () {
-    
-    /* SETTING THE STAGE */
-    
-    //setting up the question setlist
-    questionsRandom = randomizeArray(questionsOrdered);
-    whichQuestion = 0; //reset the question counter
-    //populate the quiz card with a random coding question
-    populateQuizCard();
+/* G A M E   F U N C T I O N S */   
+    function startGame () {
+        
+        /* SETTING THE STAGE */
+        
+        //setting up the question setlist
+        questionsRandom = randomizeArray(questionsOrdered);
+        whichQuestion = 0; //reset the question counter
+        //populate the quiz card with a random coding question
+        populateQuizCard();
 
-    //toggling to the quiz card
-    gamestartCardEl.setAttribute("style", "display: none");
-    gameoverCardEl.setAttribute("style", "display: none");
-    highscoresCardEl.setAttribute("style", "display: none");
-    quizCardEl.setAttribute("style", "display: flex");
-    
-    //setting up the clock
-    document.querySelector("#time-remaining").textContent = 30; //reset the clock
-    document.querySelector("#timer").setAttribute("style", "display: block;"); //displaying the timer
-    runningClock = setInterval(runClock, 1000); //starting the clock 
-}
-
-function gameOver (){
-    
-    //stop and hide clock
-    stopClock();
-    
-    //carry the timer value to the score value
-    document.querySelector("#score").textContent = document.querySelector("#time-remaining").textContent;
-
-    //toggling viewing to the gameover card
-    quizCardEl.setAttribute("style", "display: none");
-    gamestartCardEl.setAttribute("style", "display: none");
-    highscoresCardEl.setAttribute("style", "display: none");
-    gameoverCardEl.setAttribute("style", "display: flex");   
-}
-
-function runClock () {
-    
-    if (document.querySelector("#time-remaining").textContent <= 0) {
-        gameOver();
-        return;
+        //toggling to the quiz card
+        gamestartCardEl.setAttribute("style", "display: none");
+        gameoverCardEl.setAttribute("style", "display: none");
+        highscoresCardEl.setAttribute("style", "display: none");
+        quizCardEl.setAttribute("style", "display: flex");
+        
+        //setting up the clock
+        document.querySelector("#time-remaining").textContent = 30; //reset the clock
+        document.querySelector("#timer").setAttribute("style", "display: block;"); //displaying the timer
+        runningClock = setInterval(runClock, 1000); //starting the clock 
     }
 
-    document.querySelector("#time-remaining").textContent--;
-    
-    
-}
+    function gameOver (){
+        
+        //stop and hide clock
+        stopClock();
+        
+        //carry the timer value to the score value
+        document.querySelector("#score").textContent = document.querySelector("#time-remaining").textContent;
 
-function stopClock() {
-    
-    //stop and hide clock
-    clearInterval(runningClock); 
-    document.querySelector("#timer").setAttribute("style", "display: none;")
-}
-
-function populateQuizCard () { //populates the quiz card with a fresh question
-    document.querySelector("#question").textContent = questionsRandom[whichQuestion].question;
-    for (let i=0; i<answersEl.length; i++){
-        answersEl[i].textContent = questionsRandom[whichQuestion].answers[i];
-    }
-}
-
-function recordScore(event){
-    
-    event.preventDefault();
-
-    let myscore = { //capture the current score and initials for saving to local storage
-        initials: initialsEl.value,
-        score: document.querySelector("#time-remaining").textContent
+        //toggling viewing to the gameover card
+        quizCardEl.setAttribute("style", "display: none");
+        gamestartCardEl.setAttribute("style", "display: none");
+        highscoresCardEl.setAttribute("style", "display: none");
+        gameoverCardEl.setAttribute("style", "display: flex");   
     }
 
-    if (localStorage.getItem("allscores")){ //if existing scores detected
-        let allscores = JSON.parse(localStorage.getItem("allscores"));
-        allscores.push(myscore);
-        allscores = sortScores(allscores);
-        localStorage.setItem("allscores", JSON.stringify(allscores));
-    }
-    else { //if no existing scores detected
-        localStorage.setItem("allscores", JSON.stringify([myscore,])); //we have to take care to format the single score as a array of scores, to allow for the potential of future scores later
-    }
-    
-    // sortScores(allscores);
-    
-    showScores();
-}
-
-function showScores (){
-    
-    stopClock(); //if accessed while the game is running, we need to make sure the clock gets stopped
-    
-    //toggling from whichever card to to the highscores card
-    gameoverCardEl.setAttribute("style", "display: none");
-    gamestartCardEl.setAttribute("style", "display: none");
-    quizCardEl.setAttribute("style", "display: none");
-    highscoresCardEl.setAttribute("style", "display: flex");
-
-    removeChildNodes(highscoresEl); //we are about to repopulate the highscoresEl below
-    
-    if (localStorage.getItem("allscores")){ //if high scores are saved to local storage, call and display
-        let scoretransfer = JSON.parse(localStorage.getItem("allscores"));
-            for (i=0; i < scoretransfer.length; i++) {
-                highscoresEl.appendChild(document.createElement("LI"));
-                highscoresEl.children[i].textContent = scoretransfer[i].initials +": "+ scoretransfer[i].score;
-            }
-    }
-    else {
-        highscoresEl.appendChild(document.createElement("LI"));
-        highscoresEl.children[0].textContent = "none";
-    }
-}
-
-function clearScores(){
-    localStorage.removeItem("allscores");
-    removeChildNodes(highscoresEl);
-    showScores();
-}
-
-function checkAnswer(event){
-    
-    if (whichQuestion == questionsRandom.length-1){ //if there are no more questions, end the game. Prevents negative scores.
-        gameOver();
-        return; //prevent the end of this function which is out of bounds for the number of questions available
-    }
-    else if (event.target.value == questionsRandom[whichQuestion].correctAns){
-        rightAnswer();
-    }
-    else {
-        wrongAnswer();
-    }
-    
-    whichQuestion++;
-    populateQuizCard();
-
-}
-
-function wrongAnswer () {
-    popup("Incorrect!");
-    setTimeout(clearPopup, 2000); //clears the popup message after a delay
-
-    if (document.querySelector("#time-remaining").textContent-10<0) {
-        document.querySelector("#time-remaining").textContent = 0; //subtracts any remaining time from the score
-        gameOver();
-    }
-    else {
-        document.querySelector("#time-remaining").textContent-=10;
-    }
-
-}
-
-function rightAnswer(){
-    popup("Correct!");
-    setTimeout(clearPopup, 2000); //clears the popup message after a delay
-}
-
-
-
-// U T I L I T Y    F U N C T I O N S //
-// these functions perform simple utilties such as sorting not particular to gameplay
-function removeChildNodes(parentEl){
-    while (parentEl.firstChild) {
-        parentEl.removeChild(parentEl.firstChild);
-    }
-}
-
-function randomizeArray (array) { //assembles in a random order the various questions into an array, algorithm borrowed from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-
-   let currentIndex = array.length,  randomIndex;
-      
-    // While there remain elements to shuffle...
-    while (currentIndex != 0) {
-      
-          // Pick a remaining element...
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex--;
-      
-        // And swap it with the current element.
-        [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
+    function runClock () {
+        
+        if (document.querySelector("#time-remaining").textContent <= 0) {
+            gameOver();
+            return;
         }
-      
-        return array;
-}
 
-function popup(message){
-    document.querySelector("#popup").textContent = message; // write message to popup element
-    document.querySelector("#popup").setAttribute("style", "display: inline;") //unhide popup element
-}
+        document.querySelector("#time-remaining").textContent--;
+        
+        
+    }
 
-function clearPopup(){
-    document.querySelector("#popup").setAttribute("style", "display: none;")
-}
+    function stopClock() {
+        
+        //stop and hide clock
+        clearInterval(runningClock); 
+        document.querySelector("#timer").setAttribute("style", "display: none;")
+    }
 
-function sortScores(unsortedScores){ //algorithm borrowed from https://www.javascripttutorial.net/array/javascript-sort-an-array-of-objects/
-    return unsortedScores.sort((a, b) => {return b.score - a.score;
+    function populateQuizCard () { //populates the quiz card with a fresh question
+        document.querySelector("#question").textContent = questionsRandom[whichQuestion].question;
+        for (let i=0; i<answersEl.length; i++){
+            answersEl[i].textContent = questionsRandom[whichQuestion].answers[i];
+        }
+    }
+
+    function recordScore(event){
+        
+        event.preventDefault();
+
+        let myscore = { //capture the current score and initials for saving to local storage
+            initials: initialsEl.value,
+            score: document.querySelector("#time-remaining").textContent
+        }
+
+        if (localStorage.getItem("allscores")){ //if existing scores detected
+            let allscores = JSON.parse(localStorage.getItem("allscores"));
+            allscores.push(myscore);
+            allscores = sortScores(allscores);
+            localStorage.setItem("allscores", JSON.stringify(allscores));
+        }
+        else { //if no existing scores detected
+            localStorage.setItem("allscores", JSON.stringify([myscore,])); //we have to take care to format the single score as a array of scores, to allow for the potential of future scores later
+        }
+        
+        // sortScores(allscores);
+        
+        showScores();
+    }
+
+    function showScores (){
+        
+        stopClock(); //if accessed while the game is running, we need to make sure the clock gets stopped
+        
+        //toggling from whichever card to to the highscores card
+        gameoverCardEl.setAttribute("style", "display: none");
+        gamestartCardEl.setAttribute("style", "display: none");
+        quizCardEl.setAttribute("style", "display: none");
+        highscoresCardEl.setAttribute("style", "display: flex");
+
+        removeChildNodes(highscoresEl); //we are about to repopulate the highscoresEl below
+        
+        if (localStorage.getItem("allscores")){ //if high scores are saved to local storage, call and display
+            let scoretransfer = JSON.parse(localStorage.getItem("allscores"));
+                for (i=0; i < scoretransfer.length; i++) {
+                    highscoresEl.appendChild(document.createElement("LI"));
+                    highscoresEl.children[i].textContent = scoretransfer[i].initials +": "+ scoretransfer[i].score;
+                }
+        }
+        else {
+            highscoresEl.appendChild(document.createElement("LI"));
+            highscoresEl.children[0].textContent = "none";
+        }
+    }
+
+    function clearScores(){
+        localStorage.removeItem("allscores");
+        removeChildNodes(highscoresEl);
+        showScores();
+    }
+
+    function checkAnswer(event){
+        
+        if (whichQuestion == questionsRandom.length-1){ //if there are no more questions, end the game. Prevents negative scores.
+            gameOver();
+            return; //prevent the end of this function which is out of bounds for the number of questions available
+        }
+        else if (event.target.value == questionsRandom[whichQuestion].correctAns){
+            rightAnswer();
+        }
+        else {
+            wrongAnswer();
+        }
+        
+        whichQuestion++;
+        populateQuizCard();
+
+    }
+
+    function wrongAnswer () {
+        popup("Incorrect!");
+        clearPopup(); //clears the popup message after a delay
+
+        if (document.querySelector("#time-remaining").textContent-10<0) {
+            document.querySelector("#time-remaining").textContent = 0; //subtracts any remaining time from the score
+            gameOver();
+        }
+        else {
+            document.querySelector("#time-remaining").textContent-=10;
+        }
+
+    }
+
+    function rightAnswer(){
+        popup("Correct!");
+        clearPopup(); //clears the popup message after a delay
+    }
+
+
+
+// U T I L I T Y    F U N C T I O N S //     // these functions perform simple utilties such as sorting not particular to gameplay
+
+    function removeChildNodes(parentEl){
+        while (parentEl.firstChild) {
+            parentEl.removeChild(parentEl.firstChild);
+        }
+    }
+
+    function randomizeArray (array) { //assembles in a random order the various questions into an array, algorithm borrowed from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+
+    let currentIndex = array.length,  randomIndex;
+        
+        // While there remain elements to shuffle...
+        while (currentIndex != 0) {
+        
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+        
+            // And swap it with the current element.
+            [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+            }
+        
+            return array;
+    }
+
+    function popup(message){
+        document.querySelector("#popup").textContent = message; // write message to popup element
+        document.querySelector("#popup").setAttribute("style", "display: inline;") //unhide popup element
+    }
+
+    function clearPopup(){ //fades out the popup, function borrowed from https://github.com/mitchelwachtel/code-quiz/blob/main/assets/js/script.js
+        
+        let popupEl = document.querySelector("#popup");
+
+        fadeOut(popupEl);
+        function fadeOut(content) {
+            var opacity = 1;
+            let countdown2 = setInterval(function () {
+            if (opacity <= 0) {
+                clearInterval(countdown2);
+                content.setAttribute("style", "display: none;")
+                return;
+            } else {
+                content.setAttribute("style", "opacity: " + opacity);
+                opacity -= 0.1;
+            }
+            }, 75);
+        }
+        
+    }
+
+    function sortScores(unsortedScores){ //algorithm borrowed from https://www.javascripttutorial.net/array/javascript-sort-an-array-of-objects/
+        return unsortedScores.sort((a, b) => {return b.score - a.score;
     });
 }
 
